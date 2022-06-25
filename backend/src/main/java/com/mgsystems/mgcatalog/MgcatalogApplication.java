c40768963a2a1751eb3f5d1bfaf9970a2f98634a
marcelocreative <marcelolnoliveira@gmail.com>
2022-06-25 10:10:08 -0300

    Backend project created

---
+++ backend/src/main/java/com/mgsystems/mgcatalog/MgcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.mgsystems.mgcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mgsystems/mgcatalog/MgcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgsystems.mgcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
