49b9d6994750873c804fcfd3a7951a041f01998c
marcelocreative <marcelolnoliveira@gmail.com>
2022-07-15 09:47:13 -0300

    Product  insert, update, delete

--- backend/src/main/java/com/mgsystems/mgcatalog/resources/ProductResource.java
+++ backend/src/main/java/com/mgsystems/mgcatalog/resources/ProductResource.java
@@ -1,15 +1,22 @@
 package com.mgsystems.mgcatalog.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mgsystems.mgcatalog.dto.ProductDTO;
 import com.mgsystems.mgcatalog.services.ProductService;
@@ -42,6 +49,35 @@ public class ProductResource {
 		ProductDTO dto = service.finById(id);
 		
 		return ResponseEntity.ok().body(dto);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+		
+		dto = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+		
+		dto = service.update(id, dto);
+		
+		return ResponseEntity.ok(dto);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 		
 	}
 
--- backend/src/main/java/com/mgsystems/mgcatalog/services/ProductService.java
+++ backend/src/main/java/com/mgsystems/mgcatalog/services/ProductService.java
@@ -2,14 +2,20 @@ package com.mgsystems.mgcatalog.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mgsystems.mgcatalog.dto.CategoryDTO;
 import com.mgsystems.mgcatalog.dto.ProductDTO;
+import com.mgsystems.mgcatalog.entities.Category;
 import com.mgsystems.mgcatalog.entities.Product;
+import com.mgsystems.mgcatalog.repositories.CategoryRepository;
 import com.mgsystems.mgcatalog.repositories.ProductRepository;
 import com.mgsystems.mgcatalog.services.exceptions.ResourceNotFoundException;
 
@@ -18,6 +24,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -34,6 +43,64 @@ public class ProductService {
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		
 		return new ProductDTO(entity, entity.getCategories());
+		
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		
+		Product entity= new Product();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
+		
+	}
+
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		
+		try {
+			Product entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			
+			return new ProductDTO(entity);
+			
+		}catch(EntityNotFoundException e) {
+			
+			throw new ResourceNotFoundException("Id not found " +id);
+		}
+		
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		entity.setDate(dto.getDate());
+		entity.setDescription(dto.getDescription());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setName(dto.getName());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		
+		for(CategoryDTO catDto : dto.getCategories()) {
+			
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+		
+	}
+
+	public void delete(Long id) {
+		
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found "+id);
+		}
 		
 	}
 
